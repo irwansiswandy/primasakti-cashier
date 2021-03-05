847ddf90d8e2f3629bef2032184c21f5b2c59330
Irwan Siswandy <irwansiswandymks@gmail.com>
2021-03-05 19:15:00 +0700

    Payments report calculations added

--- src/components/PaymentsReport.vue
+++ src/components/PaymentsReport.vue
@@ -3,17 +3,23 @@
         <table-invoices :invoices="invoices_paid_today.data"
                         :loading="invoices_paid_today.loading">
         </table-invoices>
-        <v-card>
+        <v-card >
             <v-card-title>
                 Ringkasan
             </v-card-title>
             <v-card-text>
-                <div v-for="(payment_type, index) in payment_types" :key="index">
-                    {{ payment_type.name + ' : ' + $numeral(sumOfPaymentType(payment_type.value)).format('$0,0') + ' (' + numberOfPaymentType(payment_type.value) + ')' }}
+                <div class="font-weight-bold">
+                    Total nota hari ini ({{ number_of_today_invoices + ' nota' }}) :
+                    <br>{{ $numeral(total_today_invoices).format('$0,0') }}
                 </div>
                 <v-divider class="mt-2 mb-2"></v-divider>
                 <div class="font-weight-bold">
-                    Semua : {{ $numeral(total_paid).format('$0,0') + ' (' + number_of_paid_invoices + ')' }}
+                    Total pembayaran yang diterima hari ini ({{ number_of_paid_invoices + ' nota' }}) :
+                    <br>{{ $numeral(total_paid_invoices).format('$0,0') }}
+                </div>
+                <v-divider class="mt-2 mb-2"></v-divider>
+                <div v-for="(payment_type, index) in payment_types" :key="index">
+                    {{ payment_type.name + ' : ' + $numeral(sumOfPaymentType(payment_type.value)).format('$0,0') + ' (' + numberOfPaymentType(payment_type.value) + ')' }}
                 </div>
             </v-card-text>
         </v-card>
@@ -25,19 +31,30 @@
     import TableInvoices from '../components/TableInvoices';
 
     export default {
-        name: 'invoices-paid-today',
+        name: 'payments-report',
         components: {
             TableInvoices
         },
         computed: {
             ... mapGetters([
                 'payment_types',
+                'today_invoices',
                 'invoices_paid_today'
             ]),
+            number_of_today_invoices() {
+                return this.today_invoices.data.length;
+            },
+            total_today_invoices() {
+                let value = 0;
+                for (let i=0; i<this.today_invoices.data.length; i++) {
+                    value += Number(this.today_invoices.data[i].total);
+                }
+                return value;
+            },
             number_of_paid_invoices() {
                 return this.invoices_paid_today.data.length;
             },
-            total_paid() {
+            total_paid_invoices() {
                 let value = 0;
                 for (let i=0; i<this.invoices_paid_today.data.length; i++) {
                     value += Number(this.invoices_paid_today.data[i].total);
